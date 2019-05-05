46ba9466f0150582136bf1478a4089496ebb86b2
JunHoPark <pjh374968@gmail.com>
2019-05-05 17:00:44 +0900

    refactor: Add chatbot api call message style
    issue: #15 #16

--- src/main/java/com/chatbot/mentor/service/SocketService.java
+++ src/main/java/com/chatbot/mentor/service/SocketService.java
@@ -1,5 +1,6 @@
 package com.chatbot.mentor.service;
 
+import com.chatbot.mentor.domain.BotMessage;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
@@ -17,11 +18,22 @@ public class SocketService {
     private static final int DEFAULT_CHAT_SCRIPT_PORT = 1024;
     private static final String DEFAULT_HOST = "localhost";
 
-    public String getBotMessage(String message, String userName) {
+    // TODO 직업 코드 정규식 설정부분1
+    private static final String JOB_CODE_STRING_TEMP = "CODE 105,098";
+    // TODO 직업 코드 정규식 설정부분2
+    private static final int JOB_CODE_TEMP = 105098;
+
+    public BotMessage getBotMessage(String message, String userName) {
         verifyChatArguments(message, userName);
 
         // TODO 메세지 보내는 부분 개선필요할 것 같음 issue: https://github.com/mentorchatbot/mentor-chat-bot/issues/11
-        return getBotMessageUsingChatScriptProtocol(userName + NULL_CHAR + NULL_CHAR + message + NULL_CHAR);
+        String returnedMessage = getBotMessageUsingChatScriptProtocol(userName + NULL_CHAR + NULL_CHAR + message + NULL_CHAR);
+
+        if (checkIfExternalApiCode(returnedMessage)) {
+            return new BotMessage("버튼을 누르세요", true, JOB_CODE_TEMP);
+        }
+
+        return new BotMessage(returnedMessage, false);
     }
 
     // TODO Socket과 통신 관련 객체들이 Thread-Safe 한가?, 일단 인스턴스 변수로 올리지 않았음. 내부로직 개선 필요
@@ -50,5 +62,9 @@ public class SocketService {
         if (userName.isEmpty()) {
             throw new IllegalArgumentException("유저네임을 입력하세요");
         }
+    }
+
+    private boolean checkIfExternalApiCode(String message) {
+        return message.equals(JOB_CODE_STRING_TEMP);
     }
 }
